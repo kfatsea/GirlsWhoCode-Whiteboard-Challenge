c9662d5ae17d9614248e81599f2a3d4b3fca2596
Kona234 <20104839-Kona234@users.noreply.replit.com>
2025-02-05 10:59:21 +0000

    User checkpoint: Enhance repeatedValue function: Add input validation and improved output messages.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def repeatedValue(arr):
+  
+  # Constraints 
+  if len(arr) > 105 :
+    print("The array is too long. Provide a smaller one.")
+    return 1
+  
   for i in range(len(arr)):
+    if arr[i] < -109 or arr[i] > 109:
+      print(f"Array value outside valid range [-109, 109]\nGiven Array: {arr}")
+      break
     for j in range(i+1, len(arr)):
       if arr[i] == arr[j]:
-        print("true")
+        print(f"True: The number {arr[i]} is repeated at least once.")
         return True
-  print("false")
-  return False
+      else:
+        print("False: No repeated numbers within the array.")
+        return False
 
-arr = [1,1,1,3,3,4,3,2,4,2]
-repeatedValue(arr)
+  return 1
+
+nums = [-200,-1,1,3,3,4,3,2,4,2]
+repeatedValue(nums)
